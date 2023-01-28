5730ca0957c7647d24ecc00754bf5b0e373427c3
Vivianstar <106425568+Vivianstar@users.noreply.github.com>
2023-01-28 05:45:58 -0500

    commit

---
+++ src/test/java/com/shangan/online/trade/goods/TradeGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shangan.online.trade.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
